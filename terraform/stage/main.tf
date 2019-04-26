15899f7b66d5e7f6e5be878f0fde4052a5b2a711
Shawn Her Many Horses <shawnhmh@gmail.com>
2019-04-26 08:09:03 -0700

    WIP

--- terraform/stage/main.tf
+++ terraform/stage/main.tf
@@ -3,6 +3,16 @@ provider "heroku" {
   api_key = "${var.heroku_api_key}"
 }
 
+terraform {
+  backend "s3" {
+    bucket         = "spare-terraform"
+    key            = "spare-stage.tfstate"
+    region         = "us-west-2"
+    dynamodb_table = "spare-terraform"
+    profile        = "spare-terraform"
+  }
+}
+
 resource "heroku_app" "default" {
 
 }
