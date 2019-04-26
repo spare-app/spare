6d76a254afd29d688425d3c7bbc4721de5a5c0d8
Shawn Her Many Horses <shawnhmh@gmail.com>
2019-04-26 07:35:02 -0700

    WIP

--- terraform/remote/main.tf
+++ terraform/remote/main.tf
@@ -7,7 +7,7 @@ resource "aws_s3_bucket" "bucket" {
   bucket = "spare-terraform"
 
   tags {
-    Name = "Terraform Dev State"
+    Name = "Terraform State"
   }
 
   versioning {
@@ -25,12 +25,4 @@ resource "aws_dynamodb_table" "table" {
     name = "LockID"
     type = "S"
   }
-}
-
-module "state" {
-  source         = "github.com/confluentinc/terraform-state"
-  s3_bucket      = "spare-terraform"
-  s3_bucket_name = "Terraform Remote Storage"
-  dynamodb_table = "spare-terraform"
-  env            = "default"  # Not using environment-specific backends
 }
