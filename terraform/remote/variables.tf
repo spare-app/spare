21e6ea846259ecb306e7aa4e045ff538bec0821f
Shawn Her Many Horses <shawnhmh@gmail.com>
2019-04-26 07:39:31 -0700

    WIP

--- terraform/remote/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "aws_access_key" {
-  description = "AWS API access key with appropriate permissions"
-  default     = ""
-}
-
-variable "aws_secret_key" {
-  description = "AWS API secret key with appropriate permissions"
-  default     = ""
-}
